bbd32c517f46fd0e476e92cbc3308d249ef8e1c9
Akira Oberhofer <FeuxDev@gmail.com>
2019-02-08 13:20:58 +0100

    Update url.php

--- Airflow/files/url.php
+++ Airflow/files/url.php
@@ -2,8 +2,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <script src="./js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -21,7 +21,7 @@ if (! empty($_GET["url"])) {
     fclose($myfile);
     echo '<div class="alert alert-success" role="alert">
   <h4 class="alert-heading">Well done!</h4>
-  <p>Now start you`re ios shortcut Airflow beta!</p>
+  <p>Now start you`re ios shortcut!</p>
   <hr>
   <p class="mb-0">Hope you have a great Day.</p>
 </div>';
